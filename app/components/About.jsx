69fe7b0a184981eaf573eb16a8d6f3c46e400de4
Daniel Pape <danielwpape@gmail.com>
2016-10-16 17:45:55 +0100

    Styled weather page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,13 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props)=>{
   return(
     <div>
-      <h3>About</h3>
-      <p>This is the about page!</p>
+      <h1 className="text-center">About</h1>
+        <p>This is the first app written in React by Daniel Pape.</p>
+        <p>You can find the GitHub repository <Link to="https://github.com/danielpape/ReactWeather">here</Link></p>
+        <p>The app uses React, Babel, Foundation, jQuery, Axios and uses data provided by Open Weather Map.</p>
     </div>
   )
 };
