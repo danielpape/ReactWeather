07a3a8b8df199a8fe19496798bc36bb9f947c393
Daniel Pape <danielwpape@gmail.com>
2016-10-16 17:07:56 +0100

    Styled Top Bar 2

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,8 +2,8 @@ var React = require('react');
 var {Link,IndexLink} = require('react-router');
 
 var Nav = React.createClass({
-  onSearch :function(e){
-    e.preventdefault();
+  onSearch: function(e){
+    e.preventDefault();
     alert('Not yet wired up');
   },
   render: function(){
